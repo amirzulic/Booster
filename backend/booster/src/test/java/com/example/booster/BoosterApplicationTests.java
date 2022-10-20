e58546457fae9208015b5be7d6422443accbaa2c
amirzulic <nokia5130>
2022-10-20 18:20:44 +0200

    Initial project structure

---
+++ backend/booster/src/test/java/com/example/booster/BoosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col text-center">
+          <h1>Initial project structure of Booster</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
